00c1651d18641a0a4b913ecb220643cc38c37edd
Haki-Malai <p17mala@ionio.gr>
2021-10-25 00:40:43 +0300

    Comments and better code

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Toggle button from https://bbbootstrap.com/snippets/bootstrap-5-sidebar-menu-toggle-button-34132202
 const toggle = $('#header-toggle'),
 nav = $('#nav-bar'),
 bodypd = $('#body-pd'),
@@ -10,6 +11,18 @@ adm_lg_2 = $('#adm_logo_2')
 code_input = $('.code_input')
 containers = document.querySelectorAll('.container')
 links = document.querySelectorAll('.nav_name')
+
+
+// To add more submenus, increase the loop
+for (let i = 2; i <= 3; i++) {
+	$("#dropdown"+i).click(() => {
+		if ($('#submenu'+i).css('display') === 'none'){
+			$('#submenu'+i).css('display','block')
+		} else{
+			$('#submenu'+i).css('display','none')
+		}
+	})
+}
 
 toggle.click( ()=>{
 	// Show navbar
@@ -25,33 +38,27 @@ toggle.click( ()=>{
 
 	// Change logo
 	if (lg.css('display') != "none"){
+		// Change logo
 		lg.css('display','none')
 		exp_lg.css('display','block')
+		// Show 'administrator' title and logo
 		title.css('display','block')
 		adm_lg_2.css('display','none')
 		code_input.css('display','block')
 	} else{
+		// Does the opposite
 		lg.css('display','block')
 		exp_lg.css('display','none')
 		title.css('display','none')
 		adm_lg_2.css('display','block')
 		code_input.css('display','none')
-		$('#submenu2').css('display','none')
-		$('#submenu3').css('display','none')
+		// Hides all possibly open submenus
+		for (let i = 2; i <= 3; i++) {
+			$('#submenu'+i).css('display', 'none')
+		}
 		// Shorten
 		containers.forEach((element) => { element.classList.add('short')})
 		// Hide Letters
 		links.forEach((element) => {element.classList.add('invisible_text')})
 	}
 })
-
-// To add more submenus, increase the loop, small error will be expected
-for (let i = 2; i <= 3; i++) {
-	$("#dropdown"+i).click(() => {
-		if ($('#submenu'+i).css('display') === 'none'){
-			$('#submenu'+i).css('display','block')
-		} else{
-			$('#submenu'+i).css('display','none')
-		}
-	})
-}
